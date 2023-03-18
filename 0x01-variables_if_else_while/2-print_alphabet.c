8cbf3751e26bb5fb5a976cb2d29579ab5affc5b0
Abdoelbassel <abdo455911951@gmail.com>
2023-03-17 18:16:05 -0700

    b

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,21 +5,21 @@
  *
  * Description: print all aplhabet letters
  *
- * Return: always 0 (Success)
- */
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-char ch = 'a';
-        
-while (ch <= 'z')
-{
-putchar(ch);
-ch++;
-}
-putchar('\n');
-	
-return (0);
+	char ch = 'a';
 
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
 }
+
 
