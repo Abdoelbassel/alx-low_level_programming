92cd2f77e45e2b78ae46a977b01dcd8fd2be65f4
Abdoelbassel <abdo455911951@gmail.com>
2023-03-23 06:31:55 -0700

    my first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
- * main - Print _putchar
- * Return: Always 0
+ * main - Entry point
+ * Description: 'points _putchar to stdout'
+ * Return: always 0
  */
 int main(void)
 {
-	putchar('');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
